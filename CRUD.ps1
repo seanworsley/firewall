1adbd3a07e47dbb14b8f40dbcb86c3cf4a4c9aba
seanworsley <sean.worsley@gmail.com>
2013-10-08 21:43:09 +1100

    Basic menu added. CRUD.ps1 created.

---
+++ CRUD.ps1
@@ -0,0 +1,13 @@
+# CRUD.ps1 - Written by Sean Worsley on 8/10/2013
+# Supporting functions for adding and removing entries from the database 
+
+# addHostEntry - A function to add a host to the database
+# If supplied with a parent argument, creates relevant relationships within the database
+Function addHostEntry($ipaddr, $hostname="", $parent="") {
+	
+}
+
+# addGroupEntry - A function to add a new group
+Function addGroupEntry($name) {
+
+}
